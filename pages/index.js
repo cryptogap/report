082899fe44829bc71e8b1df7fd05d8552516950b
Crypto Gap <30596626+cryptogap@users.noreply.github.com>
2022-02-08 23:11:09 -0600

    script

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -9,11 +10,11 @@ export default function Home() {
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.executium.com/media/dist/realtime-cryptocurrency-markets/min.js"></script>
-        <script src="../scripts/scripts.js"></script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></Script>
+        <Script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></Script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
+        <Script src="https://cdn.executium.com/media/dist/realtime-cryptocurrency-markets/min.js"></Script>
+        <Script src="../scripts/scripts.js"></Script>
       </Head>
       <Header />
       <h1 className="text-3xl font-bold">Hello world!</h1>
