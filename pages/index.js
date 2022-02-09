fafa793ca95e07fe603945abca236743b4b75a1d
Crypto Gap <30596626+cryptogap@users.noreply.github.com>
2022-02-08 23:12:45 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,15 @@ export default function Home() {
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header />
+      <div>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></Script>
         <Script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></Script>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
         <Script src="https://cdn.executium.com/media/dist/realtime-cryptocurrency-markets/min.js"></Script>
         <Script src="../scripts/scripts.js"></Script>
-      </Head>
-      <Header />
+      </div>
       <h1 className="text-3xl font-bold">Hello world!</h1>
       <div className="bids-bitfinex-btcusd-price">&hellip;</div>
       <div className="bids-bitfinex-btcusd-qty">&hellip;</div>
